b3caa92271bab009b140a9f7d9baf8a142bfb081
spinstein <2717896+spinstein@users.noreply.github.com>
2025-07-02 19:03:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 添加额外软件源
+echo "src-git dockerman https://github.com/lisaac/luci-app-dockerman" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 2. 下载青龙面板镜像
+mkdir -p files/opt/docker
+docker pull whyour/qinglong:latest
+docker save whyour/qinglong:latest -o files/opt/docker/qinglong.tar
+
+# 3. 预置安全密钥
+mkdir -p files/etc/dropbear
+dropbearkey -t ed25519 -f files/etc/dropbear/dropbear_ed25519_host_key
+
+# 4. 禁用高危模块
+sed -i 's/CONFIG_PACKAGE_luci-app-upnp=y/# CONFIG_PACKAGE_luci-app-upnp is not set/' .config
